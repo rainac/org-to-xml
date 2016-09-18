7f3f2e55cb394039cafd67dc506b595edf13c5f3
rainac <johannes@johannes-willkomm.de>
2016-09-18 14:40:42 +0200

    Capture Emacs errors and print them

--- src/org-to-xml.sh
+++ src/org-to-xml.sh
@@ -5,8 +5,15 @@ ORGTOXML_HOME=${ORGTOXML_HOME:-$SRCDIR}
 
 in=$1
 
-emacs --batch -l $ORGTOXML_HOME/org-to-xml.el --file $in -f org-to-xml 2> /dev/null
+emacs --batch -l $ORGTOXML_HOME/org-to-xml.el --file $in -f org-to-xml 2> err
+res=$?
+
+if [[ "$res" != "0" ]]; then
+    echo "XML export failed"
+    cat err
+fi
 
 xml=$in.xml
 cat $xml
 rm $xml
+rm err
