ecfc51b49c4d69cf5ddb625f851840cc05e64378
rainac <johannes@johannes-willkomm.de>
2016-10-03 14:35:24 +0200

    Also write test files to tempdir

--- tests/test_parse.sh
+++ tests/test_parse.sh
@@ -6,36 +6,36 @@ TESTDIR=$(dirname $BASH_SOURCE)
 # test single org doc individually
 
 test_plain() {
-    rm -f res.xml
-    org-to-xml.sh $EXAMPLES/test.org > res.xml
-    check_well_formed res.xml
+    rm -f $tempdir/res.xml
+    org-to-xml.sh $EXAMPLES/test.org > $tempdir/res.xml
+    check_well_formed $tempdir/res.xml
 }
 
 test_outfile() {
-    rm -f res.xml
-    org-to-xml.sh $EXAMPLES/test.org res.xml
-    check_well_formed res.xml
+    rm -f $tempdir/res.xml
+    org-to-xml.sh $EXAMPLES/test.org $tempdir/res.xml
+    check_well_formed $tempdir/res.xml
 }
 
 test_outfile_option() {
-    rm -f res.xml
-    org-to-xml.sh -o res.xml $EXAMPLES/test.org
-    check_well_formed res.xml
+    rm -f $tempdir/res.xml
+    org-to-xml.sh -o $tempdir/res.xml $EXAMPLES/test.org
+    check_well_formed $tempdir/res.xml
 }
 
 test_outfile_option2() {
-    rm -f res.xml
-    org-to-xml.sh $EXAMPLES/test.org -o res.xml
-    check_well_formed res.xml
+    rm -f $tempdir/res.xml
+    org-to-xml.sh $EXAMPLES/test.org -o $tempdir/res.xml
+    check_well_formed $tempdir/res.xml
 }
 
 test_linked() {
-    rm -f res.xml
+    rm -f $tempdir/res.xml
     mkdir subdir
     ffn=$(readlink -f $EXAMPLES/test.org)
     ln -s $ffn subdir
-    org-to-xml.sh subdir/test.org > res.xml
-    check_well_formed res.xml
+    org-to-xml.sh subdir/test.org > $tempdir/res.xml
+    check_well_formed $tempdir/res.xml
     rm -rf subdir
 }
 
