e9cea3792374c9b41e92199be195b1178567c953
rainac <johannes@johannes-willkomm.de>
2016-10-03 14:26:49 +0200

    Tests write intermediate results to temp dir

--- tests/setup.sh
+++ tests/setup.sh
@@ -6,14 +6,18 @@ export PATH=$ORGTOXML_HOME:$PATH
 
 EXAMPLES=$ORGTOXML_HOME/../examples
 
+TMP=${TMP:-/tmp}
+tempdir=$TMP/o2x-tests-$$
+mkdir $tempdir
+
 do_parse_unparse() {
     inorg=$1
     fail=$2
-    org-to-xml.sh $inorg > res.xml
+    org-to-xml.sh $inorg > $tempdir/res.xml
     assertEquals "The script should exit successfully" "0" "$?"
-    orgxml-to-org.sh res.xml > res.org
+    orgxml-to-org.sh $tempdir/res.xml > $tempdir/res.org
     assertEquals "The script should exit successfully" "0" "$?"
-    diff $inorg res.org
+    diff $inorg $tempdir/res.org
     res=$?
     if [[ -z "$fail" ]]; then
         assertEquals "The org output should be identical to the input" "0" "$res"
@@ -26,4 +30,8 @@ check_well_formed() {
     inxml=$1
     xsltproc $ORGTOXML_HOME/orgxml-to-org.xsl $inxml > /dev/null
     assertEquals "The file $inxml should be well-formed XML" "0" "$?"
+}
+
+cleanup() {
+    rm -rf $tempdir
 }
